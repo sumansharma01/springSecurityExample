cc55dd3d7b24b6b85745a9d89fa132a63d1f9f6e
sumansharma01 <suman.sharma0331@gmail.com>
2021-03-02 11:51:20 +0530

    initial commit with spring-security dependency

---
+++ src/main/java/com/example/springsecurityexample/SpringsecurityexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityexampleApplication.class, args);
+    }
+
+}
